d251be1712f7f094b9266d30cd70a840f9dbb598
italoservio <italo.carvalho@infracommerce.com.br>
2021-10-24 16:08:13 -0300

    Navegation between SignIn and SignUp

--- app/src/main/java/br/com/cotemig/italo/party/SignInActivity.kt
+++ app/src/main/java/br/com/cotemig/italo/party/SignInActivity.kt
@@ -1,11 +1,23 @@
 package br.com.cotemig.italo.party
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SignInActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_sign_in)
+    this.setupActivity()
   }
+
+  private fun setupActivity() {
+    val signUpLink = findViewById<TextView>(R.id.link_sign_up)
+    signUpLink.setOnClickListener() {
+      val intent = Intent(this, SignUpActivity::class.java)
+      startActivity(intent)
+    }
+  }
+
 }
--- app/src/main/java/br/com/cotemig/italo/party/SignUpActivity.kt
+++ app/src/main/java/br/com/cotemig/italo/party/SignUpActivity.kt
@@ -1,11 +1,22 @@
 package br.com.cotemig.italo.party
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SignUpActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_sign_up)
+    this.setupActivity()
+  }
+
+  private fun setupActivity() {
+    val backToLoginLink = findViewById<TextView>(R.id.link_sign_up)
+    backToLoginLink.setOnClickListener() {
+      val intent = Intent(this, SignInActivity::class.java)
+      startActivity(intent)
+    }
   }
 }
