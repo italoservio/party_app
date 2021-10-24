1fc7855d74065bfb2be2b330b51a9bfe72f90c17
italoservio <italo.carvalho@infracommerce.com.br>
2021-10-24 15:09:45 -0300

    Project Structure

---
+++ app/src/main/java/br/com/cotemig/italo/party/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.cotemig.italo.party
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
